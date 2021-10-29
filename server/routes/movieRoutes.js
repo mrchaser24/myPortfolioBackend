c1ab856c4a2a7a64ece85c1809adaea275f468e6
mrchaser24 <chaserlovesyou@yahoo.com>
2021-10-29 13:31:54 +0800

    first setup

---
+++ server/routes/movieRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const movieController = require('../controllers/movieController');
+
+/***
+ * APP Routes
+ */
+
+router.get('/api/movies/', movieController.listMovies);
+
+module.exports = router;
