02b93f48de7391a64745f49acdd0025851d260a1
mrchaser24 <chaserlovesyou@yahoo.com>
2021-10-29 14:53:30 +0800

    save dummy input to database

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -12,7 +12,83 @@ const Movie = require('../models/movie');
  */
 
 
+
 //VIEW
 exports.listMovies = async(req, res) => {
-    res.send('Hello World');
+    
+
+
 }
+
+
+async function insertMovies() {
+    try {
+        await Movie.insertMany([
+            {
+                "name":'The A',
+                "description": "it has A",
+                "category": ["action", "drama"],
+                "thumbnail": "the big A"
+            },
+            {
+                "name":'The B',
+                "description": "it has B",
+                "category": ["action", "comedy"],
+                "thumbnail": "the big B"
+            },
+            {
+                "name":'The C',
+                "description": "it has C",
+                "category": ["comedy", "drama"],
+                "thumbnail": "the big C"
+            },
+            {
+                "name":'The D',
+                "description": "it has D",
+                "category": ["adventure", "sci-fi"],
+                "thumbnail": "the big D"
+            },
+            {
+                "name":'The E',
+                "description": "it has E",
+                "category": ["sci-fi", "drama"],
+                "thumbnail": "the big E"
+            },
+            {
+                "name":'The F',
+                "description": "it has F",
+                "category": ["fight", "adventure"],
+                "thumbnail": "the big F"
+            },
+            {
+                "name":'The G',
+                "description": "it has G",
+                "category": ["Sci-fi", "adventure"],
+                "thumbnail": "the big G"
+            },
+            {
+                "name":'The H',
+                "description": "it has H",
+                "category": ["drama", "history"],
+                "thumbnail": "the big H"
+            },
+            {
+                "name":'The I',
+                "description": "it has I",
+                "category": ["history", "adventure"],
+                "thumbnail": "the big I"
+            },
+            {
+                "name":'The J',
+                "description": "it has J",
+                "category": ["comedy", "sci-fi"],
+                "thumbnail": "the big J"
+            },
+        ]);
+    } catch (error) {
+        
+    }
+}
+
+//INSERT MOVIE
+insertMovies();
